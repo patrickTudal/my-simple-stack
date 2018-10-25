fa979583177b8d5cbd006d6de8d3dcb8222631e6
tudal <patrick.tudal@master-developpement-logiciel.fr>
2018-10-25 14:03:36 +0200

    itemTest

--- src/test/java/deqo/ptud/mysimplestack/ItemTest.java
+++ src/test/java/deqo/ptud/mysimplestack/ItemTest.java
@@ -1,17 +1,27 @@
 package deqo.ptud.mysimplestack;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ItemTest {
 
+    private Item item;
+
+    @Before
+    public void init(){
+        item = new Item(4);
+    }
+
     @Test
     public void getMonNum() {
+        assertTrue(item.getMonNum()==4);
     }
 
     @Test
     public void setMonNum() {
-        
+        item.setMonNum(8);
+        assertTrue(item.getMonNum()==8);
     }
 }
